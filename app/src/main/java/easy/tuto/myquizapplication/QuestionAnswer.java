6440cc24bff6202e9d9ac002b9882d32653bcda8
rohanguptakr <109743454+rohanguptakr@users.noreply.github.com>
2023-07-01 16:02:09 +0530

    final commit

---
+++ app/src/main/java/easy/tuto/myquizapplication/QuestionAnswer.java
@@ -0,0 +1,29 @@
+package easy.tuto.myquizapplication;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "Which company owns the android?",
+            "Which is not a programming language?",
+            "Which is the largest streaming platform?",
+            "Who is the CEO of Apple?",
+            "Which language is popular for making android applications?"
+    };
+
+    public static String choices[][] = {
+            {"Google","Apple","Nokia","Samsung"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"Youtube","Twitch","Xbox live","Floatplane"},
+            {"Steve Jobs","Tim Cook","Katherine Adams","Eddy Cue"},
+            {"Java","Kotlin","C#","Python"}
+    };
+
+    public static String correctAnswers[] = {
+            "Google",
+            "Notepad",
+            "Youtube",
+            "Tim Cook",
+            "Java"
+    };
+
+}
